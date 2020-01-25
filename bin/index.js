79e7b013c56280cb7d0395a3a55b86cdc87d7c5e
Tom Fletcher <fletcher.thg@gmail.com>
2020-01-25 15:11:12 -0500

    Added Box setting logics and commands

--- bin/index.js
+++ bin/index.js
@@ -7,6 +7,10 @@ if ( commands[0] === '--add') {
     const args = commands.splice(1);
     archive.addEntry(args)
     return
+} else if (commands[0] === '--box') {
+    const args = commands.splice(1);
+    archive.setBox(args)
+    return
 }
 if(!commands[0]){
     archive.load();
